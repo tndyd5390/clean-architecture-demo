d78b42fd62311835dfcc6ef3af77a9ef2769e441
waterdragon <tndyd5390@naver.com>
2022-02-15 21:57:32 +0900

    first commit

---
+++ src/main/java/com/demo/cleanarchitecturedemo/CleanArchitectureDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.cleanarchitecturedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanArchitectureDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CleanArchitectureDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/cleanarchitecturedemo/CleanArchitectureDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.cleanarchitecturedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanArchitectureDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
